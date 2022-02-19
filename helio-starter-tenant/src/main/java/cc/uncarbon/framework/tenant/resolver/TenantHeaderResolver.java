6799fbbe729db340b4bdb7a79991d62df2ca1441
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2022-02-19 22:00:17 +0800

    wip: 从请求头解析租户信息

--- helio-starter-tenant/src/main/java/cc/uncarbon/framework/tenant/resolver/TenantHeaderResolver.java
+++ helio-starter-tenant/src/main/java/cc/uncarbon/framework/tenant/resolver/TenantHeaderResolver.java
@@ -1,14 +1,23 @@
 package cc.uncarbon.framework.tenant.resolver;
 
 
-import cc.uncarbon.framework.core.context.TenantContext;
 import cc.uncarbon.framework.core.props.HelioProperties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 请求头中租户信息解析器
+ */
 public interface TenantHeaderResolver {
 
-    TenantContext resolve(HttpServletRequest request, HttpServletResponse response, Object handler, HelioProperties helioProperties);
+    /**
+     * 执行解析
+     * @param request HttpServlet 请求
+     * @param response HttpServlet 响应
+     * @param handler
+     * @param helioProperties
+     */
+    void resolve(HttpServletRequest request, HttpServletResponse response, Object handler, HelioProperties helioProperties);
 
 }
