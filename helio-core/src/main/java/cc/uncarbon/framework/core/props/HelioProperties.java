e6a5201b920307c2e18c86c3a8d0ec288cd95c3f
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2022-03-16 16:08:58 +0800

    feat: 去掉 Web 日志切面相关配置项

--- helio-core/src/main/java/cc/uncarbon/framework/core/props/HelioProperties.java
+++ helio-core/src/main/java/cc/uncarbon/framework/core/props/HelioProperties.java
@@ -11,8 +11,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * 配置项会被反序列化到本 Spring Bean 中
- * 可自行依赖注入后使用
+ * 配置项会被反序列化到本 Spring Bean 中，可自行依赖注入后使用
  *
  * @author Uncarbon
  */
@@ -24,7 +23,6 @@ public class HelioProperties {
     private final Crud crud = new Crud();
     private final Knife4j knife4j = new Knife4j();
     private final Tenant tenant = new Tenant();
-    private final WebLogging webLogging = new WebLogging();
 
 
     @Data
@@ -51,9 +49,7 @@ public class HelioProperties {
         private final IdGenerator idGenerator = new IdGenerator();
 
         /**
-         * 用的哪种数据库 如: mysql  postgresql
-         * 参考com.baomidou.mybatisplus.annotation.DbType
-         * 默认为mysql
+         * 用的哪种数据库 如: mysql  postgresql 参考com.baomidou.mybatisplus.annotation.DbType 默认为mysql
          */
         private String dbType = "mysql";
 
@@ -62,8 +58,7 @@ public class HelioProperties {
         public static class OptimisticLock {
 
             /**
-             * 是否启用乐观锁
-             * 默认为 true
+             * 是否启用乐观锁 默认为 true
              */
             private Boolean enabled = true;
 
@@ -73,20 +68,15 @@ public class HelioProperties {
         public static class IdGenerator {
 
             /**
-             * ID生成器策略
-             * 默认为SNOWFLAKE
-             */
-            private IdGeneratorStrategyEnum strategy = IdGeneratorStrategyEnum.SNOWFLAKE;
-
-            /**
-             * 雪花ID-起始时刻
-             * 默认为2021-01-01
+             * 雪花ID-起始时刻 默认为2021-01-01
              */
             String epochDate = "2021-01-01";
-
             /**
-             * 雪花ID-数据中心ID
-             * 默认为0
+             * ID生成器策略 默认为SNOWFLAKE
+             */
+            private IdGeneratorStrategyEnum strategy = IdGeneratorStrategyEnum.SNOWFLAKE;
+            /**
+             * 雪花ID-数据中心ID 默认为0
              */
             private Long datacenterId = 0L;
 
@@ -117,40 +107,24 @@ public class HelioProperties {
     public static class Tenant {
 
         /**
-         * 是否启用多租户
-         * 默认为 false
+         * 是否启用多租户 默认为 false
          */
         private Boolean enabled = false;
 
         /**
-         * 多租户隔离级别
-         * 默认为行级
+         * 多租户隔离级别 默认为行级
          */
         private TenantIsolateLevelEnum isolateLevel = TenantIsolateLevelEnum.LINE;
 
         /**
-         * 哪些表忽略租户隔离
-         * 仅对【行级、表级】隔离级别有效
+         * 哪些表忽略租户隔离 仅对【行级、表级】隔离级别有效
          */
         private Collection<String> ignoredTables = new LinkedHashSet<>(64);
 
         /**
-         * 特权租户ID，该租户可以无视租户SQL拦截器
-         * 目前仅对【行级】隔离级别有效
-         * 默认为 0
+         * 特权租户ID，该租户可以无视租户SQL拦截器 目前仅对【行级】隔离级别有效 默认为 0
          */
         private Long privilegedTenantId = HelioConstant.Tenant.DEFAULT_PRIVILEGED_TENANT_ID;
-
-    }
-
-    @Data
-    public static class WebLogging {
-
-        /**
-         * 是否启用 Web 请求记录
-         * 默认为 false
-         */
-        private Boolean enabledRequestLog = false;
 
     }
 }
