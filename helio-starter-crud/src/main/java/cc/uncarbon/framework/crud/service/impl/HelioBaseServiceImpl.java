4e77aea003fc07b20f63c38a385a97b30bd774c0
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2023-04-26 23:38:41 +0800

    feat: 自定义了`HelioBaseServiceImpl`的`update`重载，解决MP自带方法，自动填充字段不生效问题

--- helio-starter-crud/src/main/java/cc/uncarbon/framework/crud/service/impl/HelioBaseServiceImpl.java
+++ helio-starter-crud/src/main/java/cc/uncarbon/framework/crud/service/impl/HelioBaseServiceImpl.java
@@ -2,6 +2,7 @@ package cc.uncarbon.framework.crud.service.impl;
 
 import cc.uncarbon.framework.crud.entity.HelioBaseEntity;
 import cc.uncarbon.framework.crud.service.HelioBaseService;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.NoArgsConstructor;
@@ -15,5 +16,22 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor
 public class HelioBaseServiceImpl<M extends BaseMapper<E>, E extends HelioBaseEntity<?>> extends ServiceImpl<M, E> implements HelioBaseService<E> {
+
+    /**
+     * 解决MP自带的本方法，自动填充字段不生效问题
+     * 参考: https://cloud.tencent.com/developer/article/1930108
+     */
+    @Override
+    public boolean update(Wrapper<E> updateWrapper) {
+        E entity = updateWrapper.getEntity();
+        if (null == entity) {
+            try {
+                entity = this.currentModelClass().newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                // ignored
+            }
+        }
+        return update(entity, updateWrapper);
+    }
 
 }
