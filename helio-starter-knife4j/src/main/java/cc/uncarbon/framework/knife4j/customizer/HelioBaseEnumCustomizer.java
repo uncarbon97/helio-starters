d4ea8a7b81d9ac16b6fe3eb5184eefd9a67c5edb
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2024-02-29 22:35:39 +0800

    - 重写`HelioKnife4jAutoConfiguration`

--- helio-starter-knife4j/src/main/java/cc/uncarbon/framework/knife4j/customizer/HelioBaseEnumCustomizer.java
+++ helio-starter-knife4j/src/main/java/cc/uncarbon/framework/knife4j/customizer/HelioBaseEnumCustomizer.java
@@ -4,7 +4,10 @@ import cc.uncarbon.framework.core.enums.HelioBaseEnum;
 import com.fasterxml.jackson.databind.type.SimpleType;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.ParameterCustomizer;
 import org.springdoc.core.customizers.PropertyCustomizer;
+import org.springframework.core.MethodParameter;
 
 import java.util.Objects;
 
@@ -20,15 +23,18 @@ import java.util.Objects;
  *
  * @author Uncarbon
  */
-public class HelioBaseEnumCustomizer implements PropertyCustomizer {
+public class HelioBaseEnumCustomizer implements PropertyCustomizer, ParameterCustomizer {
 
+    /**
+     * 主要用于 @Schema 注解标记的字段
+     */
     @Override
     public Schema<?> customize(Schema property, AnnotatedType propertyType) {
         if (Objects.nonNull(property) && Objects.nonNull(propertyType)) {
             try {
                 SimpleType simpleType = (SimpleType) propertyType.getType();
                 if (HelioBaseEnum.class.isAssignableFrom(simpleType.getRawClass())) {
-                    property.setDescription(determineDescription(simpleType.getRawClass(), property));
+                    property.setDescription(determineDescription(simpleType.getRawClass(), property.getDescription()));
                 }
             } catch (Exception e) {
                 // fail to customize, ignored
@@ -38,12 +44,23 @@ public class HelioBaseEnumCustomizer implements PropertyCustomizer {
     }
 
     /**
+     * 主要用于 @Parameter 注解标记的字段
+     */
+    @Override
+    public Parameter customize(Parameter parameterModel, MethodParameter methodParameter) {
+        if (Objects.nonNull(parameterModel) && Objects.nonNull(methodParameter)
+                && HelioBaseEnum.class.isAssignableFrom(methodParameter.getParameterType())) {
+            parameterModel.setDescription(determineDescription(methodParameter.getParameterType(), parameterModel.getDescription()));
+        }
+        return parameterModel;
+    }
+
+    /**
      * 确定输出的描述文本
      */
-    protected String determineDescription(Class<?> helioBaseEnum, Schema<?> property) {
+    protected String determineDescription(Class<?> helioBaseEnum, String originalDescription) {
         // 获取枚举类中的所有枚举项
         HelioBaseEnum<?>[] enumItems = (HelioBaseEnum<?>[]) helioBaseEnum.getEnumConstants();
-        String originalDescription = property.getDescription();
 
         // 拼接描述字符串
         StringBuilder newDescription = new StringBuilder(128)
