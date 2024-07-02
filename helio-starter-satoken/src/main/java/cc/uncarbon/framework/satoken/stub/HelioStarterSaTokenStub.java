230008bc2a85beaacbbe958a395a7c604475f5ca
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2024-07-02 10:46:50 +0800

    feat: SaTokenLocalCacheDao

--- helio-starter-satoken/src/main/java/cc/uncarbon/framework/satoken/stub/HelioStarterSaTokenStub.java
+++
@@ -1,9 +0,0 @@
-package cc.uncarbon.framework.satoken.stub;
-
-/**
- * 需要一个空类以解决：Missing: no javadoc jar found in folder
- */
-public final class HelioStarterSaTokenStub {
-    private HelioStarterSaTokenStub() {
-    }
-}
