eb9c0235350d92d731700a5ea64d9507a0edc61a
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2026-09-26 15:40:40 +0800

    fix: 补充缺失的抽象父类

---
+++ helium-core-db/src/main/java/cc/uncarbon/framework/helium/db/entity/AbstractTenantGenericEntity.java
@@ -0,0 +1,28 @@
+package cc.uncarbon.framework.helium.db.entity;
+
+import cc.uncarbon.framework.helium.db.constant.EntityField;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * 通用实体类，含有【租户ID】
+ *
+ * @author Uncarbon
+ */
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@Data
+public abstract class AbstractTenantGenericEntity extends AbstractGenericEntity implements TenantEntity {
+
+    /**
+     * 租户ID
+     */
+    @Schema(description = "租户ID")
+    @TableField(value = EntityField.TENANT_ID_COLUMN, fill = FieldFill.INSERT)
+    private Long tenantId;
+
+}
